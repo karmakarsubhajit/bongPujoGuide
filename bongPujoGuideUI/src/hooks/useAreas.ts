147b75ac98073ed608c3eb044ffda795b6614547
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2026-05-31 15:48:28 +0530

    Delete bongPujoGuideUI directory

--- bongPujoGuideUI/src/hooks/useAreas.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query'
-import { fetchAreas, fetchAreaBySlug } from '../services/areas.service'
-
-export const useAreas = () =>
-  useQuery({ queryKey: ['areas'], queryFn: fetchAreas })
-
-export const useArea = (slug: string) =>
-  useQuery({ queryKey: ['area', slug], queryFn: () => fetchAreaBySlug(slug), enabled: !!slug })
--- bongPujoGuideUI/src/hooks/usePujos.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query'
-import { fetchFeaturedPujos, fetchPujoBySlug } from '../services/pujos.service'
-
-export const useFeaturedPujos = () =>
-  useQuery({ queryKey: ['featuredPujos'], queryFn: fetchFeaturedPujos })
-
-export const usePujo = (slug: string) =>
-  useQuery({ queryKey: ['pujo', slug], queryFn: () => fetchPujoBySlug(slug), enabled: !!slug })
